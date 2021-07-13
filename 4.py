1f054a4ab56121ae51f33f69aed7d0adcbb1a808
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-07-13 15:32:03 +0530

    jasd

--- 4.py
+++ 4.py
@@ -9,4 +9,18 @@ stu_score = {
     'jack':'maths',
     'ur':'chemistry'
 }
+
 st = pd.Series(stu_score)
+
+class_code = {
+    99:'physics',
+    100:'chemistry',
+    101:'english',
+    111:'history',
+    }
+
+se = pd.Series(class_code)
+
+grades = pd.Series([99,90,2,12,54,73,7])
+
+grades = grades+2
